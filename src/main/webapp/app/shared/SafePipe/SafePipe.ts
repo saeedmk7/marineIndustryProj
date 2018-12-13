504a86f529649aa73b4c4c2f4b71f7362a3f9e63
s.mokhtari <mokhtarisa@MOKHTARISA>
2018-12-13 20:41:43 +0330

    Training Movies

--- src/main/webapp/app/shared/SafePipe/SafePipe.ts
+++ src/main/webapp/app/shared/SafePipe/SafePipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer} from '@angular/platform-browser';
+import {SERVER_API_URL} from "app/app.constants";
 
 @Pipe({ name: 'safe' })
 export class SafePipe implements PipeTransform {
     constructor(private sanitizer: DomSanitizer) {}
     transform(url) {
-
+        url = SERVER_API_URL + url;
         return this.sanitizer.bypassSecurityTrustResourceUrl(url);
     }
 }
